70f532a8e474198dcd03ec669ee1fcc4af8b1f46
Grzegorz Kruk <krukg02@gmail.com>
2025-05-23 18:43:46 +0200

    if.sh

--- if.sh
+++ if.sh
@@ -3,7 +3,7 @@
 echo "Podaj imie: ";
 read imie;
 
-if [ $imie = "Grzesiu" ]; then 
+if [ "$imie" = "Grzesiu" ]; then "apostrofy pozwalaja wpisac imie zlozone z dwoch wyrazow 
 echo "Witam Cie Grzesiu";
 
 elif [ $imie = "Wiktoria" ]; then
